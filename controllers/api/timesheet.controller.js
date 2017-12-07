da9685aae95a4ff176f7c4ea4f2fbfd88447bc8f
L B Sastry <lbsastry@nibos.com>
2017-12-07 20:30:48 +0530

    Missed code

--- controllers/api/timesheet.controller.js
+++ controllers/api/timesheet.controller.js
@@ -70,7 +70,16 @@ function getTimesheet(req, res){
         });
 }
 
-
+function deleteTimesheet(req, res){
+    var userId = req.user.sub;
+    timesheetService.deleteTimesheet(req.params.id, userId)
+        .then(function (response) {
+            res.send(response);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
 
 function getMyReport(req, res) {
     timesheetService.getMine(req.user.sub)
