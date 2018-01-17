ecc74d407016c9e4af33a8516bf1355e8f9cd419
L B Sastry <lbsastry@nibos.com>
2018-01-17 16:46:24 +0530

    timesheets update

--- scripts/migration.js
+++ scripts/migration.js
@@ -10,129 +10,21 @@ db.bind('users');
 db.bind('projects');
 
 var service = {};
-service.getProjectInfoById = getProjectInfoById;
-var rowCnt = 0;
-var allProjects = [];
-db.projects.find({}).toArray(function(err, projects) {
-    allProjects = projects;
-    db.timesheets.find({}).sort({createdOn: 1}).toArray(function(err, timesheets) {
-        _.each(timesheets, function (timesheetObj) {
-            db.users.findById(timesheetObj.userId, function(err, userObj) {
-                if (err) console.log(err.name + ': ' + err.message);
-                if(userObj === null){
-                    console.log(timesheetObj);
-                }
-                if(userObj !== null && userObj.userResourceType){
-                    timesheetObj.userResourceType = userObj.userResourceType;
-                }else{
-                    timesheetObj.userResourceType = "";
-                }
-                timesheetObj.createdOn = new Date(timesheetObj.weekDate);
-                timesheetObj.updatedOn = new Date(timesheetObj.weekDate);
-                _.each(timesheetObj.projects, function (projectObj) {
-                    projectObj.sickLeaveHours = 0;
-                    projectObj.timeoffHours = 0;
-                    var billData = getProjectBillData(projectObj, timesheetObj.weekDate, userObj);
-                    projectObj.resourceType = billData.resourceType;
-                    projectObj.allocatedHours = billData.allocatedHours;
-                    projectObj.billableMaxHours = billData.billableMaxHours;
-                    if(projectObj.billableMaxHours > 0){
-                        if(projectObj.projectHours > projectObj.billableMaxHours){
-                            projectObj.billableHours = projectObj.billableMaxHours;
-                        }else{
-                            projectObj.billableHours = projectObj.projectHours;
-                        }
-                    }else{
-                        projectObj.billableHours = projectObj.projectHours;
-                    }
-                });
-                var timeoffObj = _.find(timesheetObj.projects, {projectName: 'Timeoff'});
-                if(timeoffObj && timesheetObj.projects.length > 1){
-                    timesheetObj.projects.splice(timesheetObj.projects.indexOf(timeoffObj), 1);
-                    timesheetObj.projects[0].timeoffHours = timeoffObj.projectHours;
-                    timesheetObj.projects[0].projectComment = timesheetObj.projects[0].projectComment + " Timeoff: " +timeoffObj.projectComment;
-                }
-                timesheetObj.timeoffHours = 0;
-                timesheetObj.totalHours = 0;
-                _.each(timesheetObj.projects, function (projectObj) {
-                    projectObj.businessUnit = "";
-                    var projectInfo = _.find(allProjects, {_id: projectObj.projectId});
-                    if(projectInfo && projectInfo.businessUnit){
-                        projectObj.businessUnit = projectInfo.businessUnit;
-                    }
-                    timesheetObj.totalHours += projectObj.projectHours;
-                    timesheetObj.timeoffHours += projectObj.sickLeaveHours;
-                    timesheetObj.timeoffHours += projectObj.timeoffHours;
-                });
-                db.timesheets.update({ _id: mongo.helper.toObjectID(timesheetObj._id) }, { $set: timesheetObj }, function(err, responseSheet) {
-                    if (err) console.log(err.name + ': ' + err.message);
-                    rowCnt++;
-                    console.log(timesheetObj._id+" Updated "+rowCnt);
-                });
-            });
+
+db.timesheets.find({}).sort({createdOn: -1}).toArray(function(err, timesheets) {
+    _.each(timesheets, function (timesheetObj) {
+        timesheetObj.overtimeHours = 0;
+        _.each(timesheetObj.projects, function (projectObj) {
+            projectObj.overtimeHours = 0;
+            if(projectObj.billableMaxHours > 0 && projectObj.projectHours > projectObj.billableMaxHours){
+                projectObj.overtimeHours = projectObj.projectHours - projectObj.billableMaxHours;
+            }
+            timesheetObj.overtimeHours += projectObj.overtimeHours;
+        });
+        db.timesheets.update({ _id: mongo.helper.toObjectID(timesheetObj._id) }, { '$set': timesheetObj }, function(err, response) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            console.log(timesheetObj._id);
         });
     });
 });
 
-
-function getProjectBillData(projectObj, weekDateVal, sheetUserObj) {
-    var BillData = {
-        resourceType: "buffer",
-        allocatedHours: 40,
-        billableMaxHours: 0
-    };
-    if (sheetUserObj && sheetUserObj.projects) {
-        var prjData = _.find(sheetUserObj.projects, {"projectId": projectObj.projectId+""});
-        if (prjData && prjData.billDates) {
-            var weekDate = new Date(weekDateVal);
-            _.each(prjData.billDates, function (billDate) {
-                if (billDate.start && billDate.start != "" && billDate.end && billDate.end != "") {
-                    var startDate = new Date(billDate.start);
-                    var endDate = new Date(billDate.end);
-                    if (weekDate >= startDate && weekDate <= endDate) {
-                        BillData.resourceType = billDate.resourceType;
-                        BillData.allocatedHours = billDate.allocatedHours;
-                        BillData.billableMaxHours = billDate.billableMaxHours;
-                    }
-                } else if (billDate.start && billDate.start != "") {
-                    var startDate = new Date(billDate.start);
-                    if (weekDate >= startDate) {
-                        BillData.resourceType = billDate.resourceType;
-                        BillData.allocatedHours = billDate.allocatedHours;
-                        BillData.billableMaxHours = billDate.billableMaxHours;
-                    }
-                } else if (billDate.end && billDate.end != "") {
-                    var endDate = new Date(billDate.end);
-                    if (weekDate <= endDate) {
-                        BillData.resourceType = billDate.resourceType;
-                        BillData.allocatedHours = billDate.allocatedHours;
-                        BillData.billableMaxHours = billDate.billableMaxHours;
-                    }
-                } else if (billDate.start == "" && billDate.end == "") {
-                    BillData.resourceType = billDate.resourceType;
-                    BillData.allocatedHours = billDate.allocatedHours;
-                    BillData.billableMaxHours = billDate.billableMaxHours;
-                }
-            });
-        }
-    }
-    if(!(BillData.allocatedHours >= 0)){
-        BillData.allocatedHours = 40;
-    }
-    if(!(BillData.billableMaxHours >= 0)){
-        BillData.billableMaxHours = 0;
-    }
-    return BillData;
-}
-
-function getProjectInfoById(projectId) {
-    var deferred = Q.defer();
-    db.projects.findById(projectId, function(err, projectInfo) {
-        if (projectInfo) {
-            deferred.resolve(projectInfo);
-        } else {
-            deferred.resolve([])
-        }
-    });
-    return deferred.promise;
-}
