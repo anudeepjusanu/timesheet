e5d8b15b1ea984ac6b91915e2d2f7596ba66e447
L B Sastry <sastrylal@gmail.com>
2018-06-21 22:19:46 +0530

    migration script

--- scripts/migration.js
+++ scripts/migration.js
@@ -8,20 +8,25 @@ var db = mongo.db(config.connectionString, { native_parser: true });
 db.bind('timesheets');
 db.bind('users');
 db.bind('projects');
+var http = require('http')
 
 var service = {};
 
-db.timesheets.find({}).sort({createdOn: -1}).toArray(function(err, timesheets) {
-    _.each(timesheets, function (timesheetObj) {
-        var weekDate = new Date(timesheetObj.weekDate);
-        if(weekDate.getDay()!==5){
-            //console.log(weekDate.getDay()+" "+weekDate);
-            console.log(timesheetObj);
+db.users.find({}).toArray(function(err, users) {
+    _.each(users, function (userObj) {
+        console.log(userObj.employeeId);
+        if(userObj.employeeId){
+            userObj.profileImgUrl = 'http://elogos.wavelabs.in/'+userObj.employeeId.substring(2)+'.png';
+            var options = {method: 'HEAD', host: 'elogos.wavelabs.in', port: 80, path: userObj.profileImgUrl, body: userObj};
+            var req = http.request(options, function(response) {
+                    if(response.statusCode == 200) {
+                        db.users.update({_id: mongo.helper.toObjectID(userObj._id)}, {'$set': userObj}, function (err, userResponse) {
+                            if (err) deferred.reject(err.name + ': ' + err.message);
+                            console.log(userObj.employeeId + " -- " + response.req.path);
+                        });
+                    }
+                });
+            req.end();
         }
-        /*db.timesheets.update({ _id: mongo.helper.toObjectID(timesheetObj._id) }, { '$set': timesheetObj }, function(err, response) {
-            if (err) deferred.reject(err.name + ': ' + err.message);
-            console.log(timesheetObj._id);
-        });*/
     });
 });
-
