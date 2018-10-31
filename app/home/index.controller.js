78ebf3110c5faa8b1f0cb24a82b32728a6bf00d8
L B Sastry <lbsastry@wavelabs.in>
2018-10-31 22:48:44 +0530

    changes

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -766,7 +766,7 @@
                 });
             }
         }
-        vm.employeeCategories = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8"];
+        vm.employeeCategories = ["C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"];
 
         vm.closeUser = function() {
             $uibModalInstance.close();
