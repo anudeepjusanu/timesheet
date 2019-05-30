770d757b28479d39823af04e156210f925cc8c3f
L B Sastry <lbsastry@wavelabs.in>
2019-05-30 13:12:44 +0530

    leaves

--- app/leaves/leaves.controller.js
+++ app/leaves/leaves.controller.js
@@ -38,12 +38,33 @@
     function ViewLeaveController(UserService, _, TimesheetService) {
         var vm = this;
         vm.user = {};
+        vm.myleaves = [];
 
         function getMyLeaves(){
-
-            TimesheetService.userTakenLeaves(vm.user._id).then(function(response) {
-                console.log("userTakenLeaves : " + response);
+            UserService.getMyLeaveWallet().then(function(myleaves) {
+                vm.myleaves = myleaves;
+                _.each(vm.myleaves, function(leaveObj){
+                    leaveObj.takenLeaves = 0.00;
+                    leaveObj.monthBalance = 0.00;
+                });
+                TimesheetService.userTakenLeaves(vm.user._id).then(function(leavesData) {
+                    if(leavesData){
+                        _.each(leavesData, function(leaveObj){
+                            var leaveWeek = new Date(leaveObj.weekDate);
+                            var monthNum  = leaveWeek.getMonth()+1;
+                            var yearMonth = String(leaveWeek.getFullYear()+"-"+(monthNum>9?monthNum:"0"+monthNum));
+                            var myLeaveObj = _.find(vm.myleaves, {yearMonth: yearMonth});
+                            if(myLeaveObj){
+                                var timeoffHours = parseFloat(leaveObj.timeoffHours/8);
+                                myLeaveObj.takenLeaves += timeoffHours;
+                                myLeaveObj.takenLeaves = parseFloat(parseFloat(myLeaveObj.takenLeaves).toFixed(2));
+                                myLeaveObj.monthBalance = parseFloat(myLeaveObj.accruedLeaves + myLeaveObj.creditedLeaves - myLeaveObj.takenLeaves - myLeaveObj.deductedLOP).toFixed(2);
+                            }
+                        });
+                    }
+                });
             });
+            
         }
 
         function initController() {
