d855c0009cc8c08c1b022c78bb8cbe5cf23ad8eb
Narendar Rathod <narendarn@wavelabs.in>
2019-05-30 12:17:20 +0530

    Added My Leaves tab in layout

--- app/leaves/leaves.controller.js
+++ app/leaves/leaves.controller.js
@@ -4,7 +4,8 @@
     angular
         .module('app')
         .controller('Leaves.IndexController', LeavesController)
-        .controller('Leaves.AddLeaveController', AddLeaveController);
+        .controller('Leaves.AddLeaveController', AddLeaveController)
+        .controller('Leaves.ViewLeaveController', ViewLeaveController);
 
     function LeavesController(UserService, _, SurveyService) {
         var vm = this;
@@ -29,6 +30,30 @@
                 if (vm.user.admin !== true) {
 
                 }
+            });
+        };
+        initController();
+    };
+
+    function ViewLeaveController(UserService, _, TimesheetService) {
+        var vm = this;
+        vm.user = {};
+
+        function getMyLeaves(){
+
+            TimesheetService.userTakenLeaves(vm.user._id).then(function(response) {
+                console.log("userTakenLeaves : " + response);
+            });
+        }
+
+        function initController() {
+            
+            UserService.GetCurrent().then(function(user) {
+                vm.user = user;
+                if (vm.user.admin !== true) {
+
+                }
+                getMyLeaves();
             });
         };
         initController();
