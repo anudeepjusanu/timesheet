0f85333c2e661a1f825b93bc6399ecf90f0e24c8
Narendar Rathod <narendarn@wavelabs.in>
2019-06-03 17:09:49 +0530

    Added link to my leaves tab

--- app/app.js
+++ app/app.js
@@ -270,14 +270,6 @@
                 parent: 'layout',
                 data: { activeTab: 'appsettings' }
             })
-            .state('myLeaves', {
-                url: '/myLeaves',
-                templateUrl: 'leaves/myLeaves.html',
-                controller: 'Leaves.ViewLeaveController',
-                controllerAs: 'vm',
-                parent: 'layout',
-                data: { activeTab: 'myLeaves' }
-            })
         ChartJsProvider.setOptions({ colors: ['#1caf9a', '#273541'] });
     }
 
