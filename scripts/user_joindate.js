7fcc004b2e70bb8faa73b7bfbdd9a7d96357cd10
L B Sastry <lbsastry@wavelabs.ai>
2020-07-08 16:50:05 +0530

    Join Date issue update

---
+++ scripts/user_joindate.js
@@ -0,0 +1,25 @@
+var config = require('../config.json');
+var _ = require('lodash');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('timesheets');
+db.bind('users');
+db.bind('projects');
+var http = require('http');
+
+var service = {};
+
+db.users.find({}).toArray(function(err, users) {
+    _.each(users, function (userObj) {
+        if(userObj.employeeId){
+            console.log(userObj.joinDate, typeof userObj.joinDate);
+            if(typeof userObj.joinDate == "string"){
+                db.users.update({_id: mongo.helper.toObjectID(userObj._id)}, {'$set': {joinDate: new Date(userObj.joinDate)}}, function (err, userResponse) {
+                    if (err) deferred.reject(err.name + ': ' + err.message);
+                    console.log(userObj.employeeId + " ");
+                });
+            }
+        }
+    });
+}); 
