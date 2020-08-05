25edfea430e3f1c1da2758c175567b0abbd477f4
Nagaraju Kommanaboyina <nagarajukb@wavelabs.ai>
2020-08-05 14:10:16 +0530

    Integrated API to MyReimbursement & TeamReimbursement pages

--- app/app-services/reimbursement.service.js
+++ app/app-services/reimbursement.service.js
@@ -8,6 +8,15 @@
     function ReimbursementService($http, $q) {
         var service = {};
         service.getMyReimbursements = getMyReimbursements;
+        service.getReimbursement = getReimbursement;
+        service.addReimbursement = addReimbursement;
+        // service.updateReimbursement = updateReimbursement;
+        // service.deleteReimbursement = deleteReimbursement;
+        // service.getReimbursementItem = getReimbursementItem;
+        // service.addReimbursementItem = addReimbursementItem;
+        // service.updateReimbursementItem = updateReimbursementItem;
+        // service.updateReimbursementItemFile = updateReimbursementItemFile;
+        // service.deleteReimbursementItem = deleteReimbursementItem;
 
         return service;
 
@@ -15,8 +24,16 @@
             return $http.get('/api/reimbursement/').then(handleSuccess, handleError);
         }
 
+        function getReimbursement(reimbursementId) {
+            return $http.get('/api/reimbursement/5f27c557517e74bf473d8fcc', + reimbursementId).then(handleSuccess, handleError);
+        }
+
+        function addReimbursement(reimbursementId, fileFormData, obj) {
+            return $http.post('/api/reimbursement/item/' + reimbursementId, fileFormData, obj).then(handleSuccess, handleError);
+        }
+
         // private functions
-        
+
         function handleSuccess(res) {
             return res.data;
         }
--- app/reimbursement/reimbursement.controller.js
+++ app/reimbursement/reimbursement.controller.js
@@ -23,7 +23,7 @@
              */
             link: function (scope, element, attrs) {
                 var model = $parse(attrs.fileDirective),
-                    modelSetter = model.assign; //define a setter for demoFileModel
+                    modelSetter = model.assign; //define a setter for fileDirective
 
                 //Bind change event on the element
                 element.bind('change', function () {
@@ -38,29 +38,29 @@
     }
 
     /**Service to File UPLOAD */
-    function FileUploadService($http, $q) {
-        // uploadFileToUrl = function(file, uploadUrl) {
-        uploadFileToUrl = function (file, uploadUrl) {
-            //FormData, object of key/value pair for form fields and values
-            var fileFormData = new FormData();
-            fileFormData.append('file', file);
+    // function FileUploadService($http, $q) {
+    //     // uploadFileToUrl = function(file, uploadUrl) {
+    //     uploadFileToUrl = function (file, uploadUrl) {
+    //         //FormData, object of key/value pair for form fields and values
+    //         var fileFormData = new FormData();
+    //         fileFormData.append('file', file);
 
-            var deffered = $q.defer();
-            $http.post(uploadUrl, fileFormData, {
-                transformRequest: angular.identity,
-                headers: { 'Content-Type': undefined }
+    //         var deffered = $q.defer();
+    //         $http.post(uploadUrl, fileFormData, {
+    //             transformRequest: angular.identity,
+    //             headers: { 'Content-Type': undefined }
 
-            }).success(function (response) {
-                deffered.resolve(response);
+    //         }).success(function (response) {
+    //             deffered.resolve(response);
 
-            }).error(function (response) {
-                deffered.reject(response);
-            });
+    //         }).error(function (response) {
+    //             deffered.reject(response);
+    //         });
 
-            return deffered.promise;
-        }
-        //  }
-    }
+    //         return deffered.promise;
+    //     }
+    //     //  }
+    // }
 
     function MyReimbursementsController(UserService, ReimbursementService, _, $uibModal, $filter, $state) {
         var vm = this;
@@ -115,70 +115,7 @@
         initController();
     };
 
-    function TeamReimbursementsController(UserService, _, $uibModal, $filter, $state) {
-        var vm = this;
-        vm.user = {};
-
-        vm.empReimbursementData = [
-            {
-                "Name": "Nagaraju Kommanaboyina",
-                "EmployeeID": "WL11274",
-                "AppliedDate": "01-07-2020",
-                "BusinessPurpose": "Client Meeting",
-                "FromDate": "01-06-2020",
-                "ToDate": "30-06-2020",
-                "TotalAmount": "5000"
-            },
-            {
-                "Name": "Nagaraju Kommanaboyina",
-                "EmployeeID": "WL11274",
-                "AppliedDate": "01-07-2020",
-                "BusinessPurpose": "Client Meeting",
-                "FromDate": "01-06-2020",
-                "ToDate": "30-06-2020",
-                "TotalAmount": "5000"
-            },
-            {
-                "Name": "Nagaraju Kommanaboyina",
-                "EmployeeID": "WL11274",
-                "AppliedDate": "01-07-2020",
-                "BusinessPurpose": "Client Meeting",
-                "FromDate": "01-06-2020",
-                "ToDate": "30-06-2020",
-                "TotalAmount": "5000"
-            },
-            {
-                "Name": "Nagaraju Kommanaboyina",
-                "EmployeeID": "WL11274",
-                "AppliedDate": "01-07-2020",
-                "BusinessPurpose": "Client Meeting",
-                "FromDate": "01-06-2020",
-                "ToDate": "30-06-2020",
-                "TotalAmount": "5000"
-            }
-        ];
-
-        vm.openTeamReimbursementModal = function () {
-            var modalInstance = $uibModal.open({
-                animation: true,
-                ariaLabelledBy: 'modal-title',
-                ariaDescribedBy: 'modal-body',
-                templateUrl: 'reimbursement/teamReimbursementModal.html',
-                controller: 'Reimbursement.TeamReimbursementsModalController',
-                controllerAs: 'vm',
-                size: 'lg',
-            });
-        }
-
-        function initController() {
-            UserService.GetCurrent().then(function (user) {
-                vm.user = user;
-            });
-        }
-        initController();
-    };
-
-    function AddMyReimbursementController(UserService, $scope, $timeout, _, $filter, $state) {
+    function AddMyReimbursementController(UserService, $scope, ReimbursementService, $timeout, _, $filter, $state) {
         var vm = this;
         vm.user = {};
         vm.categories = [
@@ -208,6 +145,7 @@
         };
 
         vm.changeManager = function (projectID) {
+            console.log("projectID", projectID);
             vm.myReimbursementsObj.managerName = vm.user.projects.find(obj => obj.projectId == projectID).ownerName;
         }
 
@@ -232,31 +170,57 @@
             /** File Handling **/
             var file = vm.myFile;
             console.log("file", file)
-            var uploadUrl = "../server/service.php", //Url of webservice/api/server
-                promise = FileUploadService.uploadFileToUrl(file, uploadUrl);
+            // var uploadUrl = "../server/service.php", //Url of webservice/api/server
+            //     promise = FileUploadService.uploadFileToUrl(file, uploadUrl);
 
-            promise.then(function (response) {
-                $scope.serverResponse = response;
-            }, function () {
-                $scope.serverResponse = 'An error has occurred';
-            })
-            file.upload = Upload.upload({
-                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-                data: { username: receipt.billDate, file: file },
+            // promise.then(function (response) {
+            //     $scope.serverResponse = response;
+            // }, function () {
+            //     $scope.serverResponse = 'An error has occurred';
+            // })
+            var fileFormData = new FormData();
+            fileFormData.append('file', file);
+            fileFormData.append('billDate', "2020-06-01");
+            fileFormData.append('billCategory', "Business Meals");
+            fileFormData.append('billAmount', '5000');
+            fileFormData.append('billDescription', 'Test Description');
+            for (let [name, value] of fileFormData) {
+                console.log(`${name} = ${value}`); // key1=value1, then key2=value2
+            }
+            var reimbursementId = '5f27c557517e74bf473d8fcc'
+
+            console.log("fileFormData", fileFormData);
+            console.log('formData.get(file)', fileFormData.get(file));
+            ReimbursementService.addReimbursement(reimbursementId, fileFormData, {
+                transformRequest: angular.identity,
+                headers: { 'Content-Type': undefined }
+
+            }).then(function (response) {
+                console.log("reimbursement", response);
+                vm.reimbursement = response.reimbursement;
+            }, function (error) {
+                console.log(error);
             });
 
-            file.upload.then(function (response) {
-                $timeout(function () {
-                    file.result = response.data;
-                    console.log("file.result", file.result)
-                });
-            }, function (response) {
-                if (response.status > 0)
-                    $scope.errorMsg = response.status + ': ' + response.data;
-            }, function (evt) {
-                // Math.min is to fix IE which reports 200% sometimes
-                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-            });
+
+            /**File handling code */
+            // file.upload = Upload.upload({
+            //     url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+            //     data: { username: receipt.billDate, file: file },
+            // });
+
+            // file.upload.then(function (response) {
+            //     $timeout(function () {
+            //         file.result = response.data;
+            //         console.log("file.result", file.result)
+            //     });
+            // }, function (response) {
+            //     if (response.status > 0)
+            //         $scope.errorMsg = response.status + ': ' + response.data;
+            // }, function (evt) {
+            //     // Math.min is to fix IE which reports 200% sometimes
+            //     file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            // });
 
             /** End of File Handling **/
         };
@@ -279,23 +243,23 @@
             /**File Handling **/
             var file = vm.myFile;
             console.log("file", file)
-            var uploadUrl = "../server/service.php", //Url of webservice/api/server
-                promise = fileUploadService.uploadFileToUrl(file, uploadUrl);
+            // var uploadUrl = "../server/service.php", //Url of webservice/api/server
+            //     promise = fileUploadService.uploadFileToUrl(file, uploadUrl);
 
-            promise.then(function (response) {
-                $scope.serverResponse = response;
-            }, function () {
-                $scope.serverResponse = 'An error has occurred';
-            })
+            // promise.then(function (response) {
+            //     $scope.serverResponse = response;
+            // }, function () {
+            //     $scope.serverResponse = 'An error has occurred';
+            // })
 
-            if (form.$valid) {
-                vm.enableSaveBtn = false;
-                var assignedUsers = [];
-                assignedUsers.push(vm.user);
-            } else {
-                vm.enableSaveBtn = true;
-                vm.alerts.push({ msg: "Please fill the required fields", type: 'danger' });
-            }
+            // if (form.$valid) {
+            //     vm.enableSaveBtn = false;
+            //     var assignedUsers = [];
+            //     assignedUsers.push(vm.user);
+            // } else {
+            //     vm.enableSaveBtn = true;
+            //     vm.alerts.push({ msg: "Please fill the required fields", type: 'danger' });
+            // }
         };
 
         function initController() {
@@ -314,43 +278,137 @@
         initController();
     };
 
-    function TeamReimbursementsModalController(UserService, _, $uibModal, $uibModalInstance, $filter, $state) {
+    function TeamReimbursementsController(UserService, ReimbursementService, _, $uibModal, $filter, $state) {
+        var vm = this;
+        vm.user = {};
+
+        // vm.empReimbursementData = [
+        //     {
+        //         "Name": "Nagaraju Kommanaboyina",
+        //         "EmployeeID": "WL11274",
+        //         "AppliedDate": "01-07-2020",
+        //         "BusinessPurpose": "Client Meeting",
+        //         "FromDate": "01-06-2020",
+        //         "ToDate": "30-06-2020",
+        //         "TotalAmount": "5000"
+        //     },
+        //     {
+        //         "Name": "Nagaraju Kommanaboyina",
+        //         "EmployeeID": "WL11274",
+        //         "AppliedDate": "01-07-2020",
+        //         "BusinessPurpose": "Client Meeting",
+        //         "FromDate": "01-06-2020",
+        //         "ToDate": "30-06-2020",
+        //         "TotalAmount": "5000"
+        //     },
+        //     {
+        //         "Name": "Nagaraju Kommanaboyina",
+        //         "EmployeeID": "WL11274",
+        //         "AppliedDate": "01-07-2020",
+        //         "BusinessPurpose": "Client Meeting",
+        //         "FromDate": "01-06-2020",
+        //         "ToDate": "30-06-2020",
+        //         "TotalAmount": "5000"
+        //     },
+        //     {
+        //         "Name": "Nagaraju Kommanaboyina",
+        //         "EmployeeID": "WL11274",
+        //         "AppliedDate": "01-07-2020",
+        //         "BusinessPurpose": "Client Meeting",
+        //         "FromDate": "01-06-2020",
+        //         "ToDate": "30-06-2020",
+        //         "TotalAmount": "5000"
+        //     }
+        // ];
+
+        function getMyReimbursements() {
+            ReimbursementService.getMyReimbursements().then(function (response) {
+                console.log("reimbursements", response);
+                vm.teamReimbursements = response.reimbursements;
+                console.log("vm.teamReimbursements", vm.teamReimbursements);
+                for (var i = 0; i < vm.teamReimbursements.length; i++) {
+                    vm.teamReimbursements[i].createdOn = JSON.stringify(vm.teamReimbursements[i].createdOn).split('T')[0].slice(1);
+                }
+            }, function (error) {
+                console.log(error);
+            });
+        };
+
+        vm.openTeamReimbursementModal = function (reimbursementId) {
+            console.log("reimbursementId", reimbursementId);
+            var modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'reimbursement/teamReimbursementModal.html',
+                controller: 'Reimbursement.TeamReimbursementsModalController',
+                controllerAs: 'vm',
+                size: 'lg',
+            });
+        }
+
+        function initController() {
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+            getMyReimbursements();
+        }
+        initController();
+    };
+
+    function TeamReimbursementsModalController(UserService, ReimbursementService, _, $uibModal, $uibModalInstance, $filter, $state) {
         var vm = this;
         vm.user = {};
         vm.myReimbursementsObj = {
             items: []
         };
 
-        vm.employeeReceiptData = [
-            {
-                "Date": "2020-01-06",
-                "Category": "Fuel",
-                "Cost": "1000",
-                "Description": "Test Description",
-                "Receipt": "Attachment"
-            },
-            {
-                "Date": "2020-01-06",
-                "Category": "Fuel",
-                "Cost": "1000",
-                "Description": "Test Description",
-                "Receipt": "Attachment"
-            },
-            {
-                "Date": "2020-01-06",
-                "Category": "Fuel",
-                "Cost": "1000",
-                "Description": "Test Description",
-                "Receipt": "Attachment"
-            },
-            {
-                "Date": "2020-01-06",
-                "Category": "Fuel",
-                "Cost": "1000",
-                "Description": "Test Description",
-                "Receipt": "Attachment"
-            }
-        ]
+        // vm.employeeReceiptData = [
+        //     {
+        //         "Date": "2020-01-06",
+        //         "Category": "Fuel",
+        //         "Cost": "1000",
+        //         "Description": "Test Description",
+        //         "Receipt": "Attachment"
+        //     },
+        //     {
+        //         "Date": "2020-01-06",
+        //         "Category": "Fuel",
+        //         "Cost": "1000",
+        //         "Description": "Test Description",
+        //         "Receipt": "Attachment"
+        //     },
+        //     {
+        //         "Date": "2020-01-06",
+        //         "Category": "Fuel",
+        //         "Cost": "1000",
+        //         "Description": "Test Description",
+        //         "Receipt": "Attachment"
+        //     },
+        //     {
+        //         "Date": "2020-01-06",
+        //         "Category": "Fuel",
+        //         "Cost": "1000",
+        //         "Description": "Test Description",
+        //         "Receipt": "Attachment"
+        //     }
+        // ]
+
+
+        function getMyReimbursementById() {
+            let reimbursementId = '5f27c557517e74bf473d8fcc';
+            ReimbursementService.getReimbursement(reimbursementId).then(function (response) {
+                console.log("response", response);
+                vm.reimbursement = response.reimbursement;
+                console.log("vm.reimbursement", vm.reimbursement);
+                vm.reimbursement.createdOn = JSON.stringify(vm.reimbursement.createdOn).split('T')[0].slice(1);
+                for (var i = 0; i < vm.reimbursement.items.length; i++) {
+                    vm.reimbursement.items[i].billDate = JSON.stringify(vm.reimbursement.items[i].billDate).split('T')[0].slice(1);
+                }
+            }, function (error) {
+                console.log(error);
+            });
+        };
 
         vm.accept = function (form, myReimbursementsObj) {
         };
@@ -367,6 +425,7 @@
                 vm.myReimbursementsObj.managerName = vm.user.projects[vm.user.projects.length - 1].ownerName;
                 console.log("user", user);
             });
+            getMyReimbursementById()
         }
         initController();
     };
