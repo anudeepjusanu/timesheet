1949b3117c6cf490065fbd15568b32fecd6f6688
L B Sastry <lbsastry@wavelabs.ai>
2020-08-05 17:12:06 +0530

    Changex

--- app/app-services/reimbursement.service.js
+++ app/app-services/reimbursement.service.js
@@ -10,13 +10,14 @@
         service.getMyReimbursements = getMyReimbursements;
         service.getReimbursement = getReimbursement;
         service.addReimbursement = addReimbursement;
-        // service.updateReimbursement = updateReimbursement;
-        // service.deleteReimbursement = deleteReimbursement;
-        // service.getReimbursementItem = getReimbursementItem;
-        // service.addReimbursementItem = addReimbursementItem;
-        // service.updateReimbursementItem = updateReimbursementItem;
-        // service.updateReimbursementItemFile = updateReimbursementItemFile;
-        // service.deleteReimbursementItem = deleteReimbursementItem;
+        service.updateReimbursement = updateReimbursement;
+        service.deleteReimbursement = deleteReimbursement;
+        service.getReimbursementItem = getReimbursementItem;
+        service.addReimbursementItem = addReimbursementItem;
+        service.updateReimbursementItem = updateReimbursementItem;
+        service.updateReimbursementItemFile = updateReimbursementItemFile;
+        service.deleteReimbursementItem = deleteReimbursementItem;
+        service.getReimbursementCategories = getReimbursementCategories;
 
         return service;
 
@@ -25,11 +26,43 @@
         }
 
         function getReimbursement(reimbursementId) {
-            return $http.get('/api/reimbursement/5f27c557517e74bf473d8fcc', + reimbursementId).then(handleSuccess, handleError);
+            return $http.get('/api/reimbursement/' + reimbursementId).then(handleSuccess, handleError);
         }
 
-        function addReimbursement(reimbursementId, fileFormData, obj) {
-            return $http.post('/api/reimbursement/item/' + reimbursementId, fileFormData, obj).then(handleSuccess, handleError);
+        function addReimbursement(formData) {
+            return $http.post('/api/reimbursement/', formData).then(handleSuccess, handleError);
+        }
+
+        function updateReimbursement(reimbursementId, formData) {
+            return $http.put('/api/reimbursement/' + reimbursementId, formData).then(handleSuccess, handleError);
+        }
+
+        function deleteReimbursement(reimbursementId) {
+            return $http.delete('/api/reimbursement/' + reimbursementId).then(handleSuccess, handleError);
+        }
+
+        function getReimbursementItem(itemId) {
+            return $http.get('/api/reimbursement/item/' + itemId).then(handleSuccess, handleError);
+        }
+
+        function addReimbursementItem(reimbursementId, formData) {
+            return $http.post('/api/reimbursement/item/' + reimbursementId, formData).then(handleSuccess, handleError);
+        }
+
+        function updateReimbursementItem(itemId, formData) {
+            return $http.put('/api/reimbursement/item/' + itemId, formData).then(handleSuccess, handleError);
+        }
+
+        function updateReimbursementItemFile(itemId, formData) {
+            return $http.post('/api/reimbursement/itemFile/' + itemId, formData).then(handleSuccess, handleError);
+        }
+
+        function deleteReimbursementItem(itemId) {
+            return $http.delete('/api/reimbursement/item/' + itemId).then(handleSuccess, handleError);
+        }
+
+        function getReimbursementCategories() {
+            return [];
         }
 
         // private functions
