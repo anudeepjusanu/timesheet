f7092dbbaca25743df32c0c62363aaeab708ade8
L B Sastry <lbsastry@wavelabs.ai>
2020-09-21 11:05:02 +0530

    urrent User changes

--- controllers/api/users.controller.js
+++ controllers/api/users.controller.js
@@ -85,7 +85,7 @@ function registerUser(req, res) {
 }
 
 async function getCurrentUser(req, res) {
-    userService.getById(req.user.sub).then(function (user) {
+    userService.getCurrentUser(req.user.sub).then(function (user) {
         if (user) {
             res.send(user);
         } else {
