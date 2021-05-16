8601a93f805cb496dd8929206120ebe736969775
L B Sastry <lbsastry@wavelabs.ai>
2021-05-16 12:51:38 +0530

    Cron job

--- scripts/update_emp_pratices.js
+++ scripts/update_emp_pratices.js
@@ -14,6 +14,7 @@ var employees = [];
 const fs = require('fs');
 const csv = require('csv-parser');
 const { async } = require('q');
+process.setMaxListeners(0);
 
 fs.createReadStream('./users_practice.csv').pipe(csv()).on('data', (row) => {
     employees.push(row);
@@ -21,20 +22,22 @@ fs.createReadStream('./users_practice.csv').pipe(csv()).on('data', (row) => {
     console.log('CSV file successfully processed');
 
     _.each(employees, function (empObj) {
-        db.users.find({ employeeId: empObj.emp_id }).toArray(function (err, users) {
+        //console.log(empObj);
+        db.users.find({ employeeId: empObj.emp_id }).toArray(async function (err, users) {
             if (users && users[0]) {
                 var userObj = users[0];
-
                 var projects = userObj.projects
-                var userIndex = _.findIndex(projects, { projectName: 'FirstResponder - IOT' });
-                console.log(projects[userIndex].billDates);
-                for (var i = 0; i < projects[userIndex].billDates.length; i++) {
-                    projects[userIndex].billDates[i].pratice = "Digital";
+                var userIndex = _.findIndex(projects, { projectName: empObj.project_name });
+                if (userIndex >= 0 && projects[userIndex].billDates) {
+                    console.log(userIndex, empObj.project_name);
+                    for (var i = 0; i < projects[userIndex].billDates.length; i++) {
+                        projects[userIndex].billDates[i].practice = empObj.project_users_practice;
+                    }
+                    console.log(projects[userIndex].billDates);
+                    await db.users.update({ _id: mongo.helper.toObjectID(userObj._id) }, { $set: { projects: projects } }, { upsert: true, multi: true }, function (err, doc) {
+                        console.log(userObj._id);
+                    });
                 }
-
-                db.users.update({ _id: mongo.helper.toObjectID(userObj._id) }, { $set: { projects: projects } }, { upsert: true, multi: true }, function (err, doc) {
-
-                });
             }
         });
     });
